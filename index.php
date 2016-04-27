17a0f8f723513bfbccd6bd7eba06e65f0d9c4085
ouhui <ouyangxiaohui@lingyunstrong.com>
2016-04-27 16:55:46 +0800

    monolog

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?
+<?php
+require 'vendor/autoload.php';
+
+$log = new Monolog\Logger("name");
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log' , Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
 echo "mother fucker !!!";
 ?>
